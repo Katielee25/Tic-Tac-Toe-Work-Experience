fd5d0b53631666499faeb3bce4a4cf867ee5f54a
alex <amahtab@scottlogic.com>
2026-07-14 15:50:07 +0100

    working draft

--- src/App.js
+++ src/App.js
@@ -1,36 +1,131 @@
 import React from 'react';
 import { useState } from 'react';
 
-export default function App() {
 
-  // TO DO 1:
-  // Create a state variable called squares to store the 9 board boxes.
-  // Start with an array of 9 null values.
-  // Google: "React useState array of 9"
+  function Square({id, value, onSquareClick}) {
 
-  // TO DO 2:
-  // Create state for whose turn it is.
-  // Tip: true/false can work (for example true = X, false = O).
-  // Google: "React useState boolean"
+    let content;
+    if (value === "x") {
+      content = <span className="squareItem">"x"</span> 
+    }
+    else if (value === "o") {
+      content = <span className="squareItem">"o"</span>
+    }
+    return (
+      <button id = {id} className="square" onClick={onSquareClick}>{content}
+      </button>
+    );
+  }
+ 
+  
+  function Board({xisNext, squares, onPlay}) {
 
-  // TO DO 3:
-  // Build the board UI with 9 clickable buttons.
-  // Tip: you can map over the squares array.
-  // Google: "React map array render buttons"
+    function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice()
+    if (xIsNext) {
+      nextSquares[i] = 'x';
+    } else {
+      nextSquares[i] = 'o';
+    }
+    onPlay(nextSquares);
+    }
 
-  // TO DO 4:
-  // Write a handleSquareClick function that takes the square index.
-  // Google: "React handle click with index"
+  
 
-  // TO DO 5:
-  // First rule in handleSquareClick:
-  // if the square is already filled, do nothing.
-  // Google: "JavaScript early return"
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
 
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'x' : 'o')
+  }
+
+  return (
+    <>
+    <div>
+    <div className="status">{status}</div>
+
+    <Square className ="board-row">Square value={squares[0]} on SquareClick={() => handleClick(0)} /</Square>
+    <div className ="board-row">Square value={squares[1]} on SquareClick={() => handleClick(1)} /</div>
+    <div className ="board-row">Square value={squares[2]} on SquareClick={() => handleClick(2)} /</div>
+    </div>
+     <div>
+    <div className="status">{status}</div>
+
+    <div className ="board-row">Square value={squares[3]} on SquareClick={() => handleClick(3)} /</div>
+    <div className ="board-row">Square value={squares[4]} on SquareClick={() => handleClick(4)} /</div>
+    <div className ="board-row">Square value={squares[5]} on SquareClick={() => handleClick(5)} /</div>
+    </div>
+     <div>
+    <div className="status">{status}</div>
+
+    <div className ="board-row">Square value={squares[6]} on SquareClick={() => handleClick(6)} /</div>
+    <div className ="board-row">Square value={squares[7]} on SquareClick={() => handleClick(7)} /</div>
+    <div className ="board-row">Square value={squares[8]} on SquareClick={() => handleClick(8)} /</div>
+    </div>
+    </>
+  );
+  }
+
+    function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+          return squares[a]; 
+      }
+    
+    return null;
+  
+    }
+    
+    export default  function game() {
+  const[history, setHistory] = useState([Array(9).fill(null)]);
+   const[currentMove, setCurrentMove] = useState(0);
+   const xIsNext = currentMove % 2 === 0;
+   const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'To move #' + move;
+    } else {
+      description = 'Back to Start';
+    }
+    return (
+      <li key={move}>
+        <button className="moves" onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+    });
   // TO DO 6:
   // If square is empty, copy the array,
   // put X or O in the clicked square, then save state.
   // Google: "React state immutable array update"
+
 
   // TO DO 7:
   // After placing a symbol, switch to the other player's turn.
@@ -72,9 +167,20 @@ export default function App() {
   // Replace the buttons with fun images.
 
   return (
-    <main>
+
+    <>
       <h1>Tic Tac Toe Starter</h1>
+      <div className = "gameContainer">
+      <span>title</span>
+      <div className="game">
+      <div className="game-info">
+      </div>
+      <div className="game-board"></div>
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      </div>
+
       <p>Edit App.js to build your game.</p>
-    </main>
+    </>
   );
-}
+  }
